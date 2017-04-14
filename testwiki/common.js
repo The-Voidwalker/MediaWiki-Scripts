318ed974152baaaa792ad527caed3ba98fb258e4
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2017-04-14 14:04:23 -0400

    Create common.js

---
+++ testwiki/common.js
@@ -0,0 +1,10 @@
+//Just backing up my common.js on testwiki.wiki here so that it's easier to copy-paste everything.
+mw.loader.load('//testwiki.wiki/index.php?title=User:Void/massRevdel.js&action=raw&ctype=text/javascript');
+mw.loader.load('//testwiki.wiki/index.php?title=User:Void/grantAdmin.js&action=raw&ctype=text/javascript');
+mw.loader.load('//testwiki.wiki/index.php?title=User:Void/vectorMenus.js&action=raw&ctype=text/javascript');
+mw.loader.load('//en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-popups.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-navpop.css&action=raw&ctype=text/css', 'text/css');
+mw.loader.load('//ru.wikipedia.org/w/index.php?title=MediaWiki:Gadget-markblocked.js&action=raw&ctype=text/javascript');
+mw.loader.load( '//en.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-dropdown-menus.css', 'text/css' );
+mw.loader.load('//en.wikipedia.org/w/index.php?action=raw&ctype=text/javascript&title=User:Writ_Keeper/Scripts/massRollback.js');
+mw.loader.load('//meta.miraheze.org/w/index.php?title=User:Void/filterLink.js&action=raw&ctype=text/javascript');
